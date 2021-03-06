5feab1bb90ed7923c30ebfaedef643f1c0d7f655
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2021-03-06 19:04:40 +0100

    More on collections handling

---
+++ src/app/shared/collectionItem.ts
@@ -0,0 +1,9 @@
+import {RecipeItem} from "./recipeItem";
+// TODO mettere anche l'immagine di copertina della collection come field
+export class CollectionItem {
+  recipeList: Array<RecipeItem>;
+  recipeNumber: number;
+  coverPhoto: string;
+}
+
+
