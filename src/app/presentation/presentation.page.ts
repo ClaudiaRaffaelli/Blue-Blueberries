d713e9b8c6ad76cc990b21008a035920027bb49f
abdullah <chabdullah@hotmail.it>
2021-03-09 18:01:14 +0100

    Presentation page: Implemented random picks for the user

--- src/app/presentation/presentation.page.ts
+++ src/app/presentation/presentation.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase';
+import {RecipeItemService} from '../shared/recipe-item.service';
+import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
+import {RecipeItem} from '../shared/recipeItem';
 
 @Component({
   selector: 'app-presentation',
@@ -6,18 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./presentation.page.scss'],
 })
 export class PresentationPage implements OnInit {
+  recipes = [];
+  randomSuggestions = [];
+  pathReference: any;
+  recipeKeys: [];
+  recipesRes: any;
+  imgs: []; // Title images downloaded from the firebase storage
 
   // Options for images slider
   option = {
-    slidesPerView: 1.2,
+    slidesPerView: 1.5,
     centeredSlides: true,
     loop: false,
     spaceBetween: 2,
   };
 
-  constructor() { }
+  constructor(
+      private aptService: RecipeItemService,
+      private route: ActivatedRoute,
+      private router: Router) {
+  }
+
 
   ngOnInit() {
+    this.recipesRes = this.aptService.getRecipesList();
+    this.recipesRes.snapshotChanges().subscribe(res => {
+      this.recipes = [];
+      const rndRes = [];
+      const suggestionsNumber = 3;
+      for (let i = 0; i < suggestionsNumber; i++){
+        rndRes.push(res[Math.floor(Math.random() * res.length)]); // push a random element
+      }
+      rndRes.forEach(item => {
+        const myRecipeItem = item.payload.toJSON();
+        // @ts-ignore
+        myRecipeItem.$key = item.key;
+        // get title image
+        this.pathReference = firebase.storage().ref().child(item.key + '/' + item.key + '_0.jpg').getDownloadURL().then(url => {
+          this.imgs = url;
+          // @ts-ignore
+          myRecipeItem.title_image = this.imgs;
+        });
+        this.recipes.push(myRecipeItem as RecipeItem);
+      })
+    });
+  }
+
+  openRecipe(recipeP: any){
+    const navigationExtras: NavigationExtras = {
+      state: {
+        recipe: recipeP
+      }
+    };
+    this.router.navigate(['view-recipe'], navigationExtras);
+
   }
 
 }
