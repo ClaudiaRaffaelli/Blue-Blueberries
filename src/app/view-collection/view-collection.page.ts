ed0a7e1edf90ef4a20463642524143bf38c7aac5
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2021-03-12 14:09:10 +0100

    fixed bug of duplicated un-added recipes in a collection

--- src/app/view-collection/view-collection.page.ts
+++ src/app/view-collection/view-collection.page.ts
@@ -22,36 +22,38 @@ export class ViewCollectionPage implements OnInit {
       private route: ActivatedRoute,
       private router: Router
   ) { this.route.queryParams.subscribe(async params => {
+
     if (this.router.getCurrentNavigation().extras.state) {
+      this.recipes = []
       this.collection = this.router.getCurrentNavigation().extras.state.collection;
 
-      var database = firebase.database().ref();
-      for (let recipeKey of this.collection["recipeList"]){
+        var database = firebase.database().ref();
+        for (let recipeKey of this.collection["recipeList"]){
 
-        const myRecipeItem = await database.child("recipes").child(recipeKey).get().then(function(snapshot) {
-          if (snapshot.exists()) {
-            const myRecipeItem = snapshot.val();
+          const myRecipeItem = await database.child("recipes").child(recipeKey).get().then(function(snapshot) {
+            if (snapshot.exists()) {
+              const myRecipeItem = snapshot.val();
+              // @ts-ignore
+              myRecipeItem.$key = recipeKey;
+              return myRecipeItem
+            }
+            else {
+              console.log("No data available");
+            }
+          }).catch(function(error) {
+            console.error(error);
+          });
+
+          // get title image
+          this.pathReference = firebase.storage().ref().child(recipeKey + '/' + recipeKey + '_0.jpg').getDownloadURL().then(url => {
+            this.imgs = url;
             // @ts-ignore
-            myRecipeItem.$key = recipeKey;
-            return myRecipeItem
-          }
-          else {
-            console.log("No data available");
-          }
-        }).catch(function(error) {
-          console.error(error);
-        });
-
-        // get title image
-        this.pathReference = firebase.storage().ref().child(recipeKey + '/' + recipeKey + '_0.jpg').getDownloadURL().then(url => {
-          this.imgs = url;
-          // @ts-ignore
-          myRecipeItem.title_image = this.imgs;
-        });
-        this.recipes.push(myRecipeItem as RecipeItem);
+            myRecipeItem.title_image = this.imgs;
+          });
+          this.recipes.push(myRecipeItem as RecipeItem);
+        }
       }
-    }
-  });
+    });
   }
 
   ngOnInit() {}
