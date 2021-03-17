f44f74749a885a24c121328717aaed22f7cfb6f3
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2021-03-17 18:01:54 +0100

    Added pages of settings and grocery list

---
+++ src/app/settings/settings.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+})
+export class SettingsPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  openGroceryList(){
+    const navigationExtras: NavigationExtras = {
+      state: {
+        lastPage: 'settings'
+      }
+    };
+    this.router.navigate(['groceries'], navigationExtras);
+  }
+}
+
+
