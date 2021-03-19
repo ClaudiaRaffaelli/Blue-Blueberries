23467eefea4eb76c781299669820b5865393deee
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2021-03-19 19:26:33 +0100

    Grocery list with ingredients and possibility to uncheck/check ingredients and recipes

--- src/app/groceries/groceries.page.ts
+++ src/app/groceries/groceries.page.ts
@@ -15,6 +15,8 @@ export class GroceriesPage implements OnInit {
   lastPage = '';
   recipesInGroceryList = []; // contains the list of fetched recipeItems that are present in the grocery list
   pathReference : any;
+  ingredients = [];
+  recipesCheck = []; // contains a boolean for each recipe that tells if the recipe is currently checked or not
 
   // Options for images slider
   option = {
@@ -45,6 +47,7 @@ export class GroceriesPage implements OnInit {
   async ionViewWillEnter(){
     // each time I enter the page the recipes shown in the grocery list needs to be updated
     this.recipesInGroceryList = []
+    this.recipesCheck = []
     // getting the list of keys of recipeItems from withing the Grocery List array in ionic storage
     await this.groceriesService.getGroceryList().then( async groceryList => {
       // getting the recipeItems from the list of keys
@@ -71,9 +74,42 @@ export class GroceriesPage implements OnInit {
           myRecipeItem.title_image = url;
         });
         this.recipesInGroceryList.push(myRecipeItem as RecipeItem);
+        this.recipesCheck.push(true); // as default a new recipe added is checked
+
+      }
+      this.menageIngredientsList()
+    });
+  }
+
+  menageIngredientsList(){
+    this.ingredients = [];
+
+    // putting together all the ingredients and summing the shared ingredients
+    this.recipesInGroceryList.forEach((recipe, index) =>{
+      // if the recipe is checked we show the ingredients related to it
+      if(this.recipesCheck[index] === true){
+        // going through all the ingredients
+        for (let ingredient in recipe.ingredients) {
+          // going through all the ingredients that are present in the recipe
+          if (recipe.ingredients[ingredient]["selected"] === true){
+            // we add to the ingredient list displayed this ingredient if the item is not already present
+            if (! this.ingredients.map(a=>a.name).includes(ingredient)){
+              let newIngredient = new Ingredient();
+              newIngredient.name = ingredient;
+              newIngredient.quantity = recipe.ingredients[ingredient]["dose"];
+              newIngredient.unity = recipe.ingredients[ingredient]["unit"];
+              this.ingredients.push(newIngredient);
+            }else{
+              // if already present, we only increment the quantity summing the new
+              // todo to be more precise we would have to consider different unity of the same ingredient,
+              //  but we assume that the unity is the same
+              let myIngredient = this.ingredients.find(a=>a.name === ingredient);
+              myIngredient.quantity = parseInt(myIngredient.quantity) + parseInt(recipe.ingredients[ingredient]["dose"]);
+            }
+          }
+        }
       }
     });
-
   }
 
   openRecipe(recipeP: any){
@@ -85,4 +121,35 @@ export class GroceriesPage implements OnInit {
     };
     this.router.navigate(['view-recipe'], navigationExtras);
   }
+
+  checkUncheckRecipe(recipeIndex){
+    this.recipesCheck[recipeIndex] = !this.recipesCheck[recipeIndex];
+    this.menageIngredientsList()
+  }
+
+  checkUncheckIngredient(event){
+
+    if( ! event.target.checked ){
+      event.target.parentNode.style.textDecoration = "line-through";
+      event.target.parentNode.style.opacity = 0.5;
+
+      const parent = event.target.parentElement.parentElement;
+      parent.appendChild(event.target.parentElement);
+
+    }else{
+      event.target.parentNode.style.textDecoration = "none";
+      event.target.parentNode.style.opacity = 1;
+
+      const parent = event.target.parentElement.parentElement;
+      parent.insertBefore(event.target.parentElement, parent.firstChild);
+
+    }
+  }
+}
+
+
+export class Ingredient{
+  name : string;
+  quantity : number;
+  unity: string;
 }
