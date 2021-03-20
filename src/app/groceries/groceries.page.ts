773fa6613b785e78b74fcbc9ee4238381e298177
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2021-03-20 21:11:44 +0100

    Refactoring: changed way of reloading the page after recipes insertion/deletion

--- src/app/groceries/groceries.page.ts
+++ src/app/groceries/groceries.page.ts
@@ -4,6 +4,7 @@ import {Storage} from "@ionic/storage";
 import firebase from "firebase";
 import {RecipeItem} from "../shared/recipeItem";
 import {GroceriesService} from "../shared/groceries.service";
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-groceries',
@@ -14,6 +15,9 @@ export class GroceriesPage implements OnInit {
 
   lastPage = '';
   recipesInGroceryList = []; // contains the list of fetched recipeItems that are present in the grocery list
+  oldRecipesInGroceryList = []; //list that contains the recipeItems that were in the list last time we entered the page
+  lastTimeGroceryKeyRecipes = []; // keys of the recipes that were in the list last time we entered the page
+  thisTimeGroceryKeyRecipes = [];
   pathReference : any;
   ingredients = [];
   recipesCheck = []; // contains a boolean for each recipe that tells if the recipe is currently checked or not
@@ -39,17 +43,26 @@ export class GroceriesPage implements OnInit {
         this.lastPage = this.router.getCurrentNavigation().extras.state.lastPage;
       }
     });
-  }
 
-  ngOnInit() {
-  }
+    this.recipesInGroceryList = []; // contains the list of fetched recipeItems that are present in the grocery list
+    this.oldRecipesInGroceryList = []; //list that contains the recipeItems that were in the list last time we entered the page
+    this.lastTimeGroceryKeyRecipes = []; // keys of the recipes that were in the list last time we entered the page
+    this.thisTimeGroceryKeyRecipes = [];
+    this.ingredients = [];
+    this.recipesCheck = [];
 
-  async ionViewWillEnter(){
     // each time I enter the page the recipes shown in the grocery list needs to be updated
     this.recipesInGroceryList = []
     this.recipesCheck = []
-    // getting the list of keys of recipeItems from withing the Grocery List array in ionic storage
+
+  }
+
+  async ngOnInit() {
+
+    // getting the list of keys of recipeItems from within the Grocery List array in ionic storage
     await this.groceriesService.getGroceryList().then( async groceryList => {
+      this.thisTimeGroceryKeyRecipes = groceryList;
+
       // getting the recipeItems from the list of keys
       const database = firebase.database().ref();
       for (const recipeKey of groceryList){
@@ -79,7 +92,163 @@ export class GroceriesPage implements OnInit {
       }
       this.menageIngredientsList()
     });
+
+
   }
+
+  async ionViewWillEnter(){
+    this.lastTimeGroceryKeyRecipes = this.thisTimeGroceryKeyRecipes;
+
+
+
+    await this.groceriesService.getGroceryList().then( async groceryList => {
+
+      if(groceryList.length == 0){
+        this.recipesInGroceryList = []; // contains the list of fetched recipeItems that are present in the grocery list
+        this.oldRecipesInGroceryList = []; //list that contains the recipeItems that were in the list last time we entered the page
+        this.lastTimeGroceryKeyRecipes = []; // keys of the recipes that were in the list last time we entered the page
+        this.thisTimeGroceryKeyRecipes = [];
+        this.ingredients = [];
+        this.recipesCheck = [];
+        return;
+      }
+
+      // saving the names of the recipes currently in list: they will be useful next time we enter the page
+      this.lastTimeGroceryKeyRecipes = this.thisTimeGroceryKeyRecipes;
+      // we also copy the content of what was in the grocery list last time we entered the page
+      this.oldRecipesInGroceryList = this.recipesInGroceryList
+
+      // each time I enter the page the recipes shown in the grocery list needs to be updated
+      //this.recipesInGroceryList = []
+      this.thisTimeGroceryKeyRecipes = groceryList;
+      //this.recipesCheck = []
+
+      // check if there are any differences between the recipe names before and after entering the page
+      let newRemoved = _.difference(this.lastTimeGroceryKeyRecipes, this.thisTimeGroceryKeyRecipes)
+      console.log("new removed")
+      console.log(newRemoved)
+      let newAdded = _.difference(this.thisTimeGroceryKeyRecipes, this.lastTimeGroceryKeyRecipes)
+      console.log("new added")
+      console.log(newAdded)
+
+      // getting the recipeItems from the list of keys
+      const database = firebase.database().ref();
+
+
+
+      // getting only the recipeItems for the added recipes
+      let recipeKeyIndex = 0;
+      for( let recipeKey of newAdded){
+        const recipe = await database.child('recipes').child(recipeKey).get().then(function (snapshot) {
+          if (snapshot.exists()) {
+            const recipe = snapshot.val();
+            recipe.$key = recipeKey;
+            return recipe;
+          } else {
+            console.log('No data available');
+          }
+        }).catch(function (error) {
+          console.error(error);
+        });
+
+        // get title image
+        this.pathReference = firebase.storage().ref().child(recipeKey + '/' + recipeKey + '_0.jpg').getDownloadURL().then(url => {
+          recipe.title_image = url;
+        });
+
+        //newAddedItems.push(myRecipeItem as RecipeItem);
+        this.recipesInGroceryList.push(recipe as RecipeItem);
+        this.recipesCheck.push(true); // as default a new recipe added is checked
+
+        console.log("le ricette presenti")
+        console.log(this.recipesInGroceryList)
+
+        // add the ingredients to the list of ingredients going through all of them
+        for (let ingredient in recipe.ingredients) {
+          // going through all the ingredients that are present in the recipe
+          if (recipe.ingredients[ingredient]["selected"] === true){
+            // we add to the ingredient list displayed this ingredient if the item is not already present
+            if (! this.ingredients.map(a=>a.name).includes(ingredient)){
+              let newIngredient = new Ingredient();
+              newIngredient.name = ingredient;
+              newIngredient.quantity = recipe.ingredients[ingredient]["dose"];
+              newIngredient.unity = recipe.ingredients[ingredient]["unit"];
+              this.ingredients.push(newIngredient);
+            }else{
+              // if already present, we only increment the quantity summing the new
+              // todo to be more precise we would have to consider different unity of the same ingredient,
+              //  but we assume that the unity is the same
+              let myIngredient = this.ingredients.find(a=>a.name === ingredient);
+              myIngredient.quantity = parseInt(myIngredient.quantity) + parseInt(recipe.ingredients[ingredient]["dose"]);
+            }
+          }
+        }
+        recipeKeyIndex ++;
+      }
+
+
+      // deleting the recipeItems and ingredients of the removed recipe wrt the last time we entered the page
+      for (let recipeKey of this.lastTimeGroceryKeyRecipes){
+        // if recipeKey has been deleted we fetch the recipeItem, remove the ingredients and the check from the array
+        if (!this.thisTimeGroceryKeyRecipes.includes(recipeKey)){
+          let toRemoveRecipeIndex = this.lastTimeGroceryKeyRecipes.indexOf(recipeKey)
+
+          const recipe = await database.child('recipes').child(recipeKey).get().then(function (snapshot) {
+            if (snapshot.exists()) {
+              const recipe = snapshot.val();
+              recipe.$key = recipeKey;
+              return recipe;
+            } else {
+              console.log('No data available');
+            }
+          }).catch(function (error) {
+            console.error(error);
+          });
+
+          // sets the recipeCheck to undefined. This allows to maintain the index after the delete.
+          // we will filter out the undefined when all the deleted recipes have been removed
+          delete this.recipesCheck[toRemoveRecipeIndex];
+          // same for the recipesInGroceryList
+          delete this.recipesInGroceryList[toRemoveRecipeIndex];
+
+          let ingredientIndex = 0;
+          // add the ingredients to the list of ingredients going through all of them
+          for (let ingredient in recipe.ingredients) {
+            // going through all the ingredients that are present in the recipe
+            if (recipe.ingredients[ingredient]["selected"] === true){
+
+              // the ingredient is surely in the list of ingredients and we must decrement the dose
+              let myIngredient = this.ingredients.find(a=>a.name === ingredient);
+              myIngredient.quantity = parseInt(myIngredient.quantity) - parseInt(recipe.ingredients[ingredient]["dose"]);
+
+            }
+            ingredientIndex++;
+          }
+        }
+      }
+
+      // removing the checks that were set to undefined because the relative recipe has been deleted
+      console.log(this.recipesCheck)
+      this.recipesCheck = await this.recipesCheck.filter(function (el) {
+        console.log("el")
+        console.log(el)
+        return el != undefined;
+      });
+      // if the new quantity is zero we must remove this ingredient from the list
+      this.ingredients= await this.ingredients.filter(function (el) {
+        console.log("el")
+        console.log(el)
+        return el.quantity != 0;
+      });
+      // removing the recipes from the grocery list that were set to undefined because not present anymore
+      this.recipesInGroceryList= await this.recipesInGroceryList.filter(function (el) {
+        console.log("el")
+        console.log(el)
+        return el != undefined;
+      });
+    });
+    }
+
 
   menageIngredientsList(){
     this.ingredients = [];
@@ -124,7 +293,8 @@ export class GroceriesPage implements OnInit {
 
   checkUncheckRecipe(recipeIndex){
     this.recipesCheck[recipeIndex] = !this.recipesCheck[recipeIndex];
-    this.menageIngredientsList()
+    // todo questo è da vedere
+    //this.menageIngredientsList()
   }
 
   checkUncheckIngredient(event){
@@ -152,4 +322,5 @@ export class Ingredient{
   name : string;
   quantity : number;
   unity: string;
+  checked: boolean;
 }
